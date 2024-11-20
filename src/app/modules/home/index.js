9828831edec2b18998d7c3d19b2f57678c9483e7
Ansh Varma <anshvarma83691@gmail.com>
2024-11-20 22:08:57 +0530

    record

--- src/app/modules/home/index.js
+++ src/app/modules/home/index.js
@@ -7,6 +7,10 @@ import img1 from '@/assests/speakers/speaker1.jpeg'
 import img2 from '@/assests/speakers/speaker2.jpeg'
 import img4 from '@/assests/speakers/speaker4.jpeg'
 import img5 from '@/assests/speakers/speaker5.jpeg'
+import { Building, GraduationCap, GraduationCapIcon, School, UserCircle } from 'lucide-react';
+
+
+
 
 const testimonials = [
   {
@@ -16,19 +20,19 @@ const testimonials = [
     designation: "Product Manager at TechFlow",
     src: img1
   },
-    {
+  {
     quote:
       "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
     name: "Michael Rodriguez",
     designation: "CTO at InnovateSphere",
-    src:img2
+    src: img2
   },
   {
     quote:
       "The scalability and performance have been game-changing for our organization. Highly recommend to any growing business.",
     name: "Lisa Thompson",
     designation: "VP of Technology at FutureNet",
-    src:img5
+    src: img5
   },
   {
     quote:
@@ -99,6 +103,32 @@ const Home = () => {
           </div>
         </section>
       </div>
+      <div className='bg-white flex justify-evenly rounded-lg  p-4'>
+  <div className="flex items-center">
+    <School size={32} className='mr-4' />
+    <div>
+      <p className="font-medium">24+</p>
+      <p className="text-gray-500">Colleges</p>
+    </div>
+  </div>
+  
+  <div className="flex items-center">
+    <UserCircle size={32} className="mr-4" />
+    <div>
+      <p className="font-medium">450+</p>
+      <p className="text-gray-500">Speakers</p>
+    </div>
+  </div>
+
+  <div className="flex items-center">
+    <GraduationCapIcon size={32} className="mr-4" />
+    <div>
+      <p className="font-medium">70,000+</p>
+      <p className="text-gray-500">Attendees</p>
+    </div>
+  </div>
+</div>
+
       <AnimatedTestimonials testimonials={testimonials} />;
     </section>
   );
